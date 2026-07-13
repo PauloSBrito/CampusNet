613a3401036e224553e0cea91325e008450e458f
PauloSBrito <148394940+PauloSBrito@://github.com>
2026-07-13 18:25:17 -0300

    create Conta methods

--- model/src/Conta.java
+++ model/src/Conta.java
@@ -1,4 +1,6 @@
 public class Conta {
+    private static int contadorId = 1;
+
     private int id;
     private String emailAcademico;
     private String senha;
@@ -6,8 +8,10 @@ public class Conta {
     private String grauAcademico;
     private boolean status;
 
-    public Conta(int id, String emailAcademico, String senha, String numeroTelefonico, String grauAcademico, boolean status) {
-        this.id = id;
+    public Conta(String emailAcademico, String senha, String numeroTelefonico, String grauAcademico, boolean status) {
+        this.id = contadorId;
+        contadorId++;
+
         this.emailAcademico = emailAcademico;
         this.senha = senha;
         this.numeroTelefonico = numeroTelefonico;
@@ -58,6 +62,31 @@ public class Conta {
     public void setStatus(boolean status) {
         this.status = status;
     }
+
+    public boolean validarEmail(String validacao) {
+        // Exemplo simples de regra: e-mail deve conter o domínio da universidade
+        return this.emailAcademico.contains("@") && this.emailAcademico.endsWith(".edu.br");
+    }
+
+    public boolean validarTelefone(String telefone) {
+        // Validação básica de tamanho de número telefônico
+        return telefone != null && telefone.length() >= 10;
+    }
+
+    public void alterarSenha(String novaSenha) {
+        this.senha = novaSenha;
+        System.out.println("Senha alterada com sucesso!");
+    }
+
+    public void desativarConta() {
+        this.ativa = false;
+        System.out.println("Conta desativada.");
+    }
+
+    public boolean autenticar(String senhaDigitada) {
+        return this.senha.equals(senhaDigitada);
+    }
+
 
 
 }
