8b93dd88496e159bac05c1953e5aae8f58ce12a3
PauloSBrito <148394940+PauloSBrito@://github.com>
2026-07-16 23:30:56 -0300

    Create Comentario class methods

--- model/src/Comentario.java
+++ model/src/Comentario.java
@@ -1,3 +1,36 @@
-public class Comentario {
+import java.time.LocalDateTime;
 
+public class Comentario {
+    private static int contadorId = 1;
+
+    private int id;
+    private String texto;
+    private Estudante autor;
+    private Publicacao publicacao;
+    private LocalDateTime dataComentario;
+
+    public Comentario(String texto, Estudante autor, Publicacao publicacao) {
+        this.id = contadorId++;
+        this.texto = texto;
+        this.autor = autor;
+        this.publicacao = publicacao;
+        this.dataComentario = LocalDateTime.now();
+    }
+
+    // --- Métodos do Diagrama ---
+
+    public void editarTexto(String novoTexto) {
+        this.texto = novoTexto;
+        System.out.println("Comentário editado.");
+    }
+
+    public void apagarTexto() {
+        this.texto = "[Comentário removido]";
+        System.out.println("O texto do comentário foi apagado.");
+    }
+
+    // --- Getters ---
+    public int getId() { return id; }
+    public String getTexto() { return texto; }
+    public Estudante getAutor() { return autor; }
 }
